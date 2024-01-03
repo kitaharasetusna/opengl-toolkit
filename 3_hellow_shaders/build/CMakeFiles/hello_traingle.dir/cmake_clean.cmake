9268d518565c11c69f4d9d36e2098482c8b9b4b1
kitaharasetusna <tlou2sucks@163.com>
2024-01-02 20:48:33 -0500

    update hellow shader

---
+++ 3_hellow_shaders/build/CMakeFiles/hello_traingle.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_traingle.dir/src/glad.c.o"
+  "CMakeFiles/hello_traingle.dir/src/glad.c.o.d"
+  "CMakeFiles/hello_traingle.dir/src/main.cpp.o"
+  "CMakeFiles/hello_traingle.dir/src/main.cpp.o.d"
+  "hello_traingle"
+  "hello_traingle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/hello_traingle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
