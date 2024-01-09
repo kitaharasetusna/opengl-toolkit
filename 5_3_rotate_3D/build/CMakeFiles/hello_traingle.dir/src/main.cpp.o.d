39c29d75bbae08ef83a98c25176fa725f07ecb71
kitaharasetusna <tlou2sucks@163.com>
2024-01-08 20:03:36 -0500

    update 3D

---
+++ 5_3_rotate_3D/build/CMakeFiles/hello_traingle.dir/src/main.cpp.o.d
@@ -0,0 +1,311 @@
+CMakeFiles/hello_traingle.dir/src/main.cpp.o: \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/src/main.cpp \
+ /usr/include/stdc-predef.h /usr/include/c++/10/iostream \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/os_defines.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/cpu_defines.h \
+ /usr/include/c++/10/ostream /usr/include/c++/10/ios \
+ /usr/include/c++/10/iosfwd /usr/include/c++/10/bits/stringfwd.h \
+ /usr/include/c++/10/bits/memoryfwd.h /usr/include/c++/10/bits/postypes.h \
+ /usr/include/c++/10/cwchar /usr/include/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/c++/10/exception /usr/include/c++/10/bits/exception.h \
+ /usr/include/c++/10/bits/exception_ptr.h \
+ /usr/include/c++/10/bits/exception_defines.h \
+ /usr/include/c++/10/bits/cxxabi_init_exception.h \
+ /usr/include/c++/10/typeinfo /usr/include/c++/10/bits/hash_bytes.h \
+ /usr/include/c++/10/new /usr/include/c++/10/bits/nested_exception.h \
+ /usr/include/c++/10/bits/move.h /usr/include/c++/10/type_traits \
+ /usr/include/c++/10/bits/char_traits.h \
+ /usr/include/c++/10/bits/stl_algobase.h \
+ /usr/include/c++/10/bits/functexcept.h \
+ /usr/include/c++/10/bits/cpp_type_traits.h \
+ /usr/include/c++/10/ext/type_traits.h \
+ /usr/include/c++/10/ext/numeric_traits.h \
+ /usr/include/c++/10/bits/stl_pair.h \
+ /usr/include/c++/10/bits/stl_iterator_base_types.h \
+ /usr/include/c++/10/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/10/bits/concept_check.h \
+ /usr/include/c++/10/debug/assertions.h \
+ /usr/include/c++/10/bits/stl_iterator.h \
+ /usr/include/c++/10/bits/ptr_traits.h /usr/include/c++/10/debug/debug.h \
+ /usr/include/c++/10/bits/predefined_ops.h /usr/include/c++/10/cstdint \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/c++/10/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/c++locale.h \
+ /usr/include/c++/10/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/10/cctype \
+ /usr/include/ctype.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap-16.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/c++/10/bits/ios_base.h /usr/include/c++/10/ext/atomicity.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/gthr.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/gthr-default.h \
+ /usr/include/pthread.h /usr/include/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/sched.h \
+ /usr/include/x86_64-linux-gnu/bits/cpu-set.h /usr/include/time.h \
+ /usr/include/x86_64-linux-gnu/bits/time.h \
+ /usr/include/x86_64-linux-gnu/bits/timex.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_tm.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_itimerspec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/setjmp.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/atomic_word.h \
+ /usr/include/c++/10/bits/locale_classes.h /usr/include/c++/10/string \
+ /usr/include/c++/10/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/c++allocator.h \
+ /usr/include/c++/10/ext/new_allocator.h \
+ /usr/include/c++/10/bits/ostream_insert.h \
+ /usr/include/c++/10/bits/cxxabi_forced.h \
+ /usr/include/c++/10/bits/stl_function.h \
+ /usr/include/c++/10/backward/binders.h \
+ /usr/include/c++/10/bits/range_access.h \
+ /usr/include/c++/10/initializer_list \
+ /usr/include/c++/10/bits/iterator_concepts.h \
+ /usr/include/c++/10/concepts /usr/include/c++/10/bits/range_cmp.h \
+ /usr/include/c++/10/bits/basic_string.h \
+ /usr/include/c++/10/ext/alloc_traits.h \
+ /usr/include/c++/10/bits/alloc_traits.h \
+ /usr/include/c++/10/bits/stl_construct.h \
+ /usr/include/c++/10/ext/string_conversions.h /usr/include/c++/10/cstdlib \
+ /usr/include/stdlib.h /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/sys/sysmacros.h \
+ /usr/include/x86_64-linux-gnu/bits/sysmacros.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/10/bits/std_abs.h /usr/include/c++/10/cstdio \
+ /usr/include/stdio.h /usr/include/x86_64-linux-gnu/bits/libio.h \
+ /usr/include/x86_64-linux-gnu/bits/_G_config.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/c++/10/cerrno /usr/include/errno.h \
+ /usr/include/x86_64-linux-gnu/bits/errno.h /usr/include/linux/errno.h \
+ /usr/include/x86_64-linux-gnu/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/c++/10/bits/charconv.h \
+ /usr/include/c++/10/bits/functional_hash.h \
+ /usr/include/c++/10/bits/basic_string.tcc \
+ /usr/include/c++/10/bits/locale_classes.tcc \
+ /usr/include/c++/10/system_error \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/error_constants.h \
+ /usr/include/c++/10/stdexcept /usr/include/c++/10/streambuf \
+ /usr/include/c++/10/bits/streambuf.tcc \
+ /usr/include/c++/10/bits/basic_ios.h \
+ /usr/include/c++/10/bits/locale_facets.h /usr/include/c++/10/cwctype \
+ /usr/include/wctype.h /usr/include/x86_64-linux-gnu/bits/wctype-wchar.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/ctype_base.h \
+ /usr/include/c++/10/bits/streambuf_iterator.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/ctype_inline.h \
+ /usr/include/c++/10/bits/locale_facets.tcc \
+ /usr/include/c++/10/bits/basic_ios.tcc \
+ /usr/include/c++/10/bits/ostream.tcc /usr/include/c++/10/istream \
+ /usr/include/c++/10/bits/istream.tcc \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/src/glad.h \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/src/khrplatform.h \
+ /usr/include/GLFW/glfw3.h /usr/include/GL/gl.h \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/includes/utils_glfw.h \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/includes/shader_s.h \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/includes/../src/glad.h \
+ /usr/include/c++/10/fstream /usr/include/c++/10/bits/codecvt.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/basic_file.h \
+ /usr/include/x86_64-linux-gnu/c++/10/bits/c++io.h \
+ /usr/include/c++/10/bits/fstream.tcc /usr/include/c++/10/sstream \
+ /usr/include/c++/10/bits/sstream.tcc \
+ /home/bear/Desktop/Github/opengl-toolkit/5_3_rotate_3D/includes/stb_image.h \
+ /usr/include/c++/10/stdlib.h /usr/include/string.h \
+ /usr/include/strings.h /usr/lib/gcc/x86_64-linux-gnu/10/include/limits.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/syslimits.h \
+ /usr/include/limits.h /usr/include/x86_64-linux-gnu/bits/posix1_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/local_lim.h \
+ /usr/include/linux/limits.h \
+ /usr/include/x86_64-linux-gnu/bits/posix2_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/xopen_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/uio_lim.h /usr/include/c++/10/math.h \
+ /usr/include/c++/10/cmath /usr/include/math.h \
+ /usr/include/x86_64-linux-gnu/bits/math-vector.h \
+ /usr/include/x86_64-linux-gnu/bits/libm-simd-decl-stubs.h \
+ /usr/include/x86_64-linux-gnu/bits/flt-eval-method.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-logb.h \
+ /usr/include/x86_64-linux-gnu/bits/fp-fast.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls-helper-functions.h \
+ /usr/include/x86_64-linux-gnu/bits/mathcalls.h \
+ /usr/include/x86_64-linux-gnu/bits/iscanonical.h /usr/include/assert.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/mm_malloc.h \
+ /usr/include/glm/glm.hpp /usr/include/glm/detail/_fixes.hpp \
+ /usr/include/glm/detail/setup.hpp /usr/include/c++/10/cassert \
+ /usr/include/c++/10/cstddef /usr/include/glm/simd/platform.h \
+ /usr/include/c++/10/climits /usr/include/c++/10/cfloat \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/float.h \
+ /usr/include/c++/10/limits /usr/include/glm/fwd.hpp \
+ /usr/include/glm/detail/type_int.hpp /usr/include/glm/detail/setup.hpp \
+ /usr/include/glm/detail/type_float.hpp \
+ /usr/include/glm/detail/type_vec.hpp \
+ /usr/include/glm/detail/qualifier.hpp \
+ /usr/include/glm/detail/compute_vector_relational.hpp \
+ /usr/include/c++/10/cstring /usr/include/glm/detail/type_mat.hpp \
+ /usr/include/glm/vec2.hpp /usr/include/glm/detail/type_vec2.hpp \
+ /usr/include/glm/detail/type_vec2.inl /usr/include/glm/vec3.hpp \
+ /usr/include/glm/detail/type_vec3.hpp \
+ /usr/include/glm/detail/type_vec3.inl /usr/include/glm/vec4.hpp \
+ /usr/include/glm/detail/type_vec4.hpp \
+ /usr/include/glm/detail/type_vec4.inl \
+ /usr/include/glm/detail/type_vec4_simd.inl /usr/include/glm/mat2x2.hpp \
+ /usr/include/glm/detail/type_mat2x2.hpp \
+ /usr/include/glm/detail/type_mat2x2.inl /usr/include/glm/matrix.hpp \
+ /usr/include/glm/mat2x3.hpp /usr/include/glm/detail/type_mat2x3.hpp \
+ /usr/include/glm/detail/type_mat2x3.inl /usr/include/glm/mat2x4.hpp \
+ /usr/include/glm/detail/type_mat2x4.hpp \
+ /usr/include/glm/detail/type_mat2x4.inl /usr/include/glm/mat3x2.hpp \
+ /usr/include/glm/detail/type_mat3x2.hpp \
+ /usr/include/glm/detail/type_mat3x2.inl /usr/include/glm/mat3x3.hpp \
+ /usr/include/glm/detail/type_mat3x3.hpp \
+ /usr/include/glm/detail/type_mat3x3.inl /usr/include/glm/mat3x4.hpp \
+ /usr/include/glm/detail/type_mat3x4.hpp \
+ /usr/include/glm/detail/type_mat3x4.inl /usr/include/glm/mat4x2.hpp \
+ /usr/include/glm/detail/type_mat4x2.hpp \
+ /usr/include/glm/detail/type_mat4x2.inl /usr/include/glm/mat4x3.hpp \
+ /usr/include/glm/detail/type_mat4x3.hpp \
+ /usr/include/glm/detail/type_mat4x3.inl /usr/include/glm/mat4x4.hpp \
+ /usr/include/glm/detail/type_mat4x4.hpp \
+ /usr/include/glm/detail/type_mat4x4.inl \
+ /usr/include/glm/detail/type_mat4x4_simd.inl \
+ /usr/include/glm/detail/func_matrix.inl /usr/include/glm/geometric.hpp \
+ /usr/include/glm/detail/func_geometric.inl \
+ /usr/include/glm/exponential.hpp /usr/include/glm/detail/type_vec1.hpp \
+ /usr/include/glm/detail/func_exponential.inl \
+ /usr/include/glm/vector_relational.hpp \
+ /usr/include/glm/detail/func_vector_relational.inl \
+ /usr/include/glm/detail/func_vector_relational_simd.inl \
+ /usr/include/glm/detail/_vectorize.hpp \
+ /usr/include/glm/detail/type_vec1.hpp \
+ /usr/include/glm/detail/func_exponential_simd.inl \
+ /usr/include/glm/simd/exponential.h /usr/include/glm/common.hpp \
+ /usr/include/glm/detail/func_common.inl \
+ /usr/include/glm/detail/func_common_simd.inl \
+ /usr/include/glm/simd/common.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xsaveintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xsaveoptintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xsavesintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xsavecintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512fintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512erintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512pfintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512cdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512bwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512dqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vlbwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vldqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512ifmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512ifmavlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vbmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vbmivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx5124fmapsintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx5124vnniwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vpopcntdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vbmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vbmi2vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vnniintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vnnivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vpopcntdqvlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512bitalgintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vp2intersectintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512vp2intersectvlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/shaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/xtestintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/cetintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/gfniintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/vaesintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/vpclmulqdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/movdirintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/sgxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/pconfigintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/waitpkgintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/cldemoteintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512bf16vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/avx512bf16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/enqcmdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/rdseedintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/prfchwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/adxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/clwbintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/clflushoptintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/wbnoinvdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/10/include/pkuintrin.h \
+ /usr/include/glm/detail/func_geometric_simd.inl \
+ /usr/include/glm/simd/geometric.h \
+ /usr/include/glm/detail/func_matrix_simd.inl \
+ /usr/include/glm/simd/matrix.h /usr/include/glm/trigonometric.hpp \
+ /usr/include/glm/detail/func_trigonometric.inl \
+ /usr/include/glm/detail/func_trigonometric_simd.inl \
+ /usr/include/glm/packing.hpp /usr/include/glm/detail/func_packing.inl \
+ /usr/include/glm/detail/type_half.hpp \
+ /usr/include/glm/detail/type_half.inl \
+ /usr/include/glm/detail/func_packing_simd.inl \
+ /usr/include/glm/integer.hpp /usr/include/glm/detail/func_integer.inl \
+ /usr/include/glm/ext/vec1.hpp /usr/include/glm/detail/type_vec1.inl \
+ /usr/include/glm/detail/func_integer_simd.inl \
+ /usr/include/glm/simd/integer.h \
+ /usr/include/glm/gtc/matrix_transform.hpp \
+ /usr/include/glm/gtc/constants.hpp /usr/include/glm/detail/setup.hpp \
+ /usr/include/glm/gtc/constants.inl \
+ /usr/include/glm/gtc/matrix_transform.inl \
+ /usr/include/glm/gtc/type_ptr.hpp /usr/include/glm/gtc/quaternion.hpp \
+ /usr/include/glm/gtc/quaternion.inl /usr/include/glm/gtc/epsilon.hpp \
+ /usr/include/glm/gtc/epsilon.inl \
+ /usr/include/glm/gtc/quaternion_simd.inl /usr/include/glm/gtc/vec1.hpp \
+ /usr/include/glm/gtc/vec1.inl /usr/include/glm/gtc/type_ptr.inl
